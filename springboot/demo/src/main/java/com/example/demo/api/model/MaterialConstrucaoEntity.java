c4baa0f4b4846ebf16eddb10dc58db03586a02f5
Marco Tulio Soares <110120983+MarcoTulioSoares@users.noreply.github.com>
2025-09-27 18:22:15 -0300

    Adiciona script SQL para garantir criação das tabelas

--- springboot/demo/src/main/java/com/example/demo/api/model/MaterialConstrucaoEntity.java
+++ springboot/demo/src/main/java/com/example/demo/api/model/MaterialConstrucaoEntity.java
@@ -6,34 +6,34 @@ import lombok.*;
 
 @Getter @Setter @Builder @NoArgsConstructor @AllArgsConstructor
 @Entity
-@Table(name = "TB_MATERIAL_CONSTRUCAO")
+@Table(name = "tb_material_construcao")
 public class MaterialConstrucaoEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "CODIGO_MATERIAL", nullable = false, updatable = false,
+    @Column(name = "codigo_material", nullable = false, updatable = false,
             columnDefinition = "INT AUTO_INCREMENT")
     private Integer codMaterial;
 
-    @Column(name = "CODIGO_PRODUTO", length = 50, nullable = false)
+    @Column(name = "codigo_produto", length = 50, nullable = false)
     private String codigoProduto;
 
-    @Column(name = "VALOR", nullable = false)
+    @Column(name = "valor", nullable = false)
     private Double valor;
 
-    @Column(name = "COR", length = 50)
+    @Column(name = "cor", length = 50)
     private String cor;
 
-    @Column(name = "NOME", length = 150, nullable = false)
+    @Column(name = "nome", length = 150, nullable = false)
     private String nome;
 
-    @Column(name = "MATERIA_PRIMA", length = 100)
+    @Column(name = "materia_prima", length = 100)
     private String materiaPrima;
 
     // relação com filial
     @JsonBackReference("filial-materiais")
     @ManyToOne(optional = false, fetch = FetchType.LAZY)
-    @JoinColumn(name = "CODIGO_FILIAL", nullable = false,
-            foreignKey = @ForeignKey(name = "FK_MATERIAL_FILIAL"))
+    @JoinColumn(name = "codigo_filial", nullable = false,
+            foreignKey = @ForeignKey(name = "fk_material_filial"))
     private FilialEntity filial;
 }
